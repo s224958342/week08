bf3d3ff1444663d35aa098c26f752e6abdfbf4dc
Jessica <s224958342@deakin.edu.au>
2026-09-10 17:15:14 +1000

    Update visible frontend heading for deployment demo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
             fontWeight: 600,
           }}
         >
-          KoalaTech University
+          KoalaTech University - Auto Deployment
         </Typography>
 
         <Box
